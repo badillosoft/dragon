58bc6186f419836b4d10b56ab4ad87e0f921e335
badillosoft <badillo.soft@hotmail.com>
2019-09-26 22:22:54 -0500

    zen.ui v1.2.0 fix7

--- v1/zen.ui.js
+++ v1/zen.ui.js
@@ -155,18 +155,26 @@ async function loadComponent(url, state = null) {
             }
         }
 
-        state.notify = $control => {
-            console.log(`control`, name, element.dataset.id, $control);
-            $control.dataset.id = element.dataset.id;
-            // control.ref._tid[element.dataset.id] = $control;
-            $control.bind.state$notify = updateState => {
-                console.log(`update control`, name, $control, $control.dataset.id);
-                Object.assign($component.state, updateState);
-                $control.fire.initialize = true;
-            };
-        };
+        // state.notify = $control => {
+        //     console.log(`control`, name, element.dataset.id, $control);
+        //     $control.dataset.id = element.dataset.id;
+        //     // control.ref._tid[element.dataset.id] = $control;
+        //     $control.bind.state$notify = updateState => {
+        //         console.log(`update control`, name, $control, $control.dataset.id);
+        //         Object.assign($control.state, updateState);
+        //         $control.fire.initialize = true;
+        //     };
+        // };
         
-        const _component = component(name, state);
+        // const _component = component(name, state);
+        // _component.dataset.tid = element.dataset.id;
+        // control.ref._control[name] = _component;
+
+        const _component = component(name, Object.assign(state, {
+            notify($control) {
+                console.log(`control`, name, element.dataset.id, $control);
+            }
+        }));
         _component.dataset.tid = element.dataset.id;
         control.ref._control[name] = _component;
     });
