c81cfdedf8d94755f58569721b993bfcf94133ca
badillosoft <badillo.soft@hotmail.com>
2019-09-27 00:02:06 -0500

    zen.ui v1.2.6 fix7

--- v1/zen.ui.js
+++ v1/zen.ui.js
@@ -178,6 +178,7 @@ async function loadComponent(url, state = null) {
             $control.bind.state = newState => {
                 console.log(`parent`, $control, $control.parentElement);
                 if (!document.body.ref._uid[$control.dataset.uid]) {
+                    $control.state = {};
                     _component.remove();
                     $control.remove();
                     console.log(`parent removed`, $control, $control.parentElement);
